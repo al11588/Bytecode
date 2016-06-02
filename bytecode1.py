4b8f40889a9864886dd3aa26d7435a66b38b08be
Alvin Lawson <al11588@gmail.com>
2016-06-02 13:35:05 -0400

    fixed code and added the readme

--- bytecode1.py
+++ bytecode1.py
@@ -1,9 +1,9 @@
 import dis #importing the dissambler 
 
-def add_with_assign(g, e, d): #defined my function to be g e d
+def addanddivide(g, e, d): #defined my function to be g e d
 
 	x = g + e / d # adding g and e then dividing d
 	return x # returning the x value
 
-dis.dis(add_with_assign) #output the function in binary code
+dis.dis(addanddivide) #output the function in binary code
 
